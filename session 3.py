249925781abc5ac4771850ae2b094fb59c22c098
leticiabonjean <lpedrosadosg.ieu2023@student.ie.edu>
2025-01-15 16:41:15 +0100

    my second commit to this file

--- session 3.py
+++ session 3.py
@@ -1,8 +1,13 @@
 print("hello world")
 print("maria belen perez")
 # red file means not good
-# green file good
+# green file good = brand new
 # you have to do this every time you create a new project + and a new file in the project
+# commit is like saving the file, you don't do it every second, but wnv you do something meaningful
+
+print("another line of text")
+# blue file = new changes that haven't been commited yet
+
 
 
 
