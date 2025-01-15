70a600e61a85705473b6775687a9d955280bf7ca
leticiabonjean <lpedrosadosg.ieu2023@student.ie.edu>
2025-01-15 16:37:58 +0100

    my first commit to this file

---
+++ session 3.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("maria belen perez")
+# red file means not good
+# green file good
+# you have to do this every time you create a new project + and a new file in the project
+
+
+
+
